3d3b698f28e3e60f076e155cdda82c23ec838c91
yuikoito <toffywing@gmail.com>
2021-07-12 14:05:08 +0900

    add components

---
+++ components/Loading.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full mx-auto">
+    <div class="pokemon w-full mx-auto"></div>
+  </div>
+</template>
+<style>
+.pokemon {
+  height: 60px;
+  width: 60px;
+  position: relative;
+}
+.pokemon::before,
+.pokemon::after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  border-radius: 50%;
+  transform-origin: bottom;
+}
+.pokemon::after {
+  background: radial-gradient(at 75% 15%, #fffb, #0000 35%),
+    radial-gradient(at 80% 40%, #0000, #0008),
+    radial-gradient(circle 5px, #fff 94%, #0000),
+    radial-gradient(circle 10px, #000 94%, #0000),
+    linear-gradient(#f93318 0 0) top / 100% calc(50% - 5px),
+    linear-gradient(#fff 0 0) bottom/100% calc(50% - 5px) #000;
+  background-repeat: no-repeat;
+  animation: w10 1s infinite cubic-bezier(0.5, 120, 0.5, -120);
+}
+
+.pokemon::before {
+  background: #ddd;
+  filter: blur(8px);
+  transform: scaleY(0.4) translate(-13px, 0px);
+}
+
+@keyframes w10 {
+  30%,
+  70% {
+    transform: rotate(0deg);
+  }
+  49.99% {
+    transform: rotate(0.2deg);
+  }
+  50% {
+    transform: rotate(-0.2deg);
+  }
+}
+</style>
